e9aa11924457a3cd371f457c2d8b30ac403e7970
logopek <49311518+logopek@users.noreply.github.com>
2022-04-10 21:45:45 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def add_point(user_data):
 
 
 def create_painting(user_data):
-    with dpg.window(label='second'):
+    with dpg.window(label='second',no_resize=True, no_close=True, no_move=True, no_title_bar=True, no_collapse=True):
         with dpg.drawlist(width=600, height=600):
             for i in range(1, count):
                 x = [int(i)*5 for i in dpg.get_value(item=f'c_{i}').split()]
@@ -27,13 +27,13 @@ def create_painting(user_data):
                 dpg.draw_line(p1=x, p2=y, color=(255, 255, 255), thickness=2)
 
 
-with dpg.window(label='Function', tag='Main', width=600, height=600):
+with dpg.window(label='Function', tag='Main', width=600, height=600, no_resize=True, no_close=True, no_move=True, no_title_bar=True, no_collapse=True):
     dpg.add_button(label='Create Painting', callback=create_painting)
     dpg.add_input_text(label='Dot 1', tag='c_1', width=100)
     dpg.add_input_text(label='Dot 2', tag='c_2', width=100)
     dpg.add_button(label='Add dot', callback=add_point, user_data='Main', tag='btn_close')
 
-dpg.create_viewport(title='Custom Title', width=600, height=600)
+dpg.create_viewport(title='Paintinger', width=600, height=600, resizable=False)
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.start_dearpygui()
