2104c15d8abc3fab62ac19e3fbebd5474992b706
logopek <49311518+logopek@users.noreply.github.com>
2022-04-10 21:53:00 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-
+from dearpygui.demo import show_demo
 import dearpygui.dearpygui as dpg
 
 dpg.create_context()
 count = 2
-last_pos = []
 
 
-def add_point(user_data):
+def add_point(sender, app_data, user_data):
     if dpg.get_value('mode') == 'default graphics':
         pass
     else:
@@ -15,25 +14,25 @@ def add_point(user_data):
         dpg.add_input_text(label=f"Dot {count}", parent=user_data, tag=f'c_{count}', width=100)
 
         dpg.delete_item(item='btn_close')
-        dpg.add_button(label='Add dot', callback=add_point, user_data='Main', tag='btn_close', parent=user_data)
+        dpg.add_button(label='Add point', callback=add_point, user_data='Main', tag='btn_close', parent=user_data)
 
 
-def create_painting(user_data):
-    with dpg.window(label='second',no_resize=True, no_move=True, no_collapse=True):
+def create_function(user_data):
+    with dpg.window(label='second'):
         with dpg.drawlist(width=600, height=600):
             for i in range(1, count):
-                x = [int(i)*5 for i in dpg.get_value(item=f'c_{i}').split()]
-                y = [int(i)*5 for i in dpg.get_value(item=f'c_{i + 1}').split()]
+                x = [int(i) for i in dpg.get_value(item=f'c_{i}').split()]
+                y = [int(i) for i in dpg.get_value(item=f'c_{i + 1}').split()]
                 dpg.draw_line(p1=x, p2=y, color=(255, 255, 255), thickness=2)
 
 
-with dpg.window(label='Function', tag='Main', width=600, height=600, no_resize=True, no_move=True, no_title_bar=True, no_collapse=True):
-    dpg.add_button(label='Create Painting', callback=create_painting)
+with dpg.window(label='Function', tag='Main', width=600, height=600):
+    dpg.add_button(label='Create Function', callback=create_function)
     dpg.add_input_text(label='Dot 1', tag='c_1', width=100)
     dpg.add_input_text(label='Dot 2', tag='c_2', width=100)
-    dpg.add_button(label='Add dot', callback=add_point, user_data='Main', tag='btn_close')
+    dpg.add_button(label='Add Point', callback=add_point, user_data='Main', tag='btn_close')
 
-dpg.create_viewport(title='Paintinger', width=600, height=600, resizable=False)
+dpg.create_viewport(title='Custom Title', width=600, height=600)
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.start_dearpygui()
